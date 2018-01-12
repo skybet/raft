d68faad9cf2c527a45625dcd67c163152c812a2b
Zhuang Weiming <wingalong@gmail.com>
2018-01-12 11:31:11 +0800

    change import pkg from hashicorp to weimingwill

--- fuzzy/cluster.go
+++ fuzzy/cluster.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/hashicorp/raft"
+	"github.com/weimingwill/raft"
 )
 
 type appliedItem struct {
--- fuzzy/fsm.go
+++ fuzzy/fsm.go
@@ -8,7 +8,7 @@ import (
 	"io"
 	"os"
 
-	"github.com/hashicorp/raft"
+	"github.com/weimingwill/raft"
 )
 
 type logHash struct {
